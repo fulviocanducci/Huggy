756dd44736fc8ac713fcd6b17b086087e68ae451
fulviocanducci <fulviocanducci@hotmail.com>
2024-10-25 16:10:13 -0300

    up0001

--- HuggyConsole/Program.cs
+++ HuggyConsole/Program.cs
@@ -11,57 +11,59 @@ namespace HuggyConsole
          IHttpClientHuggy httpClientHuggy = new HttpClientHuggy(new HttpClient(), configurationHuggy);
          //IReturnOf<Agent> resultById = httpClientHuggy.Agent.GetAsync(153603).Result;
          //IReturnOf<List<AgentList>> resultList = httpClientHuggy.Agent.GetListAsync().Result;
-         //try
-         //{
-         //   var model = new Huggy.Models.Flows.Flow()
-         //   {
-         //      Uuid = "1fb9f1bf-242f-4229-a238-6a6d9995af43",
-         //      Variables = new Dictionary<string, string>
-         //      {
-         //         ["link"] = "https://www.s2milhas.com.br",
-         //         ["name"] = "Fúlvio Cezar Canducci Dias"
-         //      },
-         //      WhenInAuto = true,
-         //      WhenInChat = true,
-         //      WhenWaitForChat = true
-         //   };
-         //   var result = httpClientHuggy.Flows.PostAsync("422482", "94331700", model).Result;
+         try
+         {
+            var model = new Huggy.Models.Flows.Flow()
+            {
+               Uuid = "1fb9f1bf-242f-4229-a238-6a6d9995af43",
+               Variables = new Dictionary<string, string>
+               {
+                  ["name"] = "Fúlvio Cezar Canducci Dias",
+                  ["resume"] = $"Passagem Ida LIS x GRU dia 10/01/2025, Companhia TAP com 3 Passageiros",
+                  ["paid"] = "Pago: Entrada de R$ 1000 e 5 boletos de R$ 500",
+                  ["boletos"] = "[1] Boleto - https://www.s2milhas.com.br, [2] Boleto - https://www.s2milhas.com.br"
+               },
+               WhenInAuto = true,
+               WhenInChat = true,
+               WhenWaitForChat = true
+            };
+            var result = httpClientHuggy.Flows.PostAsync("422648", "94364563", model).Result;
 
-         //   Console.WriteLine(result.StatusCode);
-         //   Console.WriteLine(result.Data.ChatID);
-         //   Console.WriteLine(result.Data.Reason);
-         //}
-         //catch (Exception)
-         //{
+            Console.WriteLine(result.StatusCode);
+            Console.WriteLine(result.Data.ChatID);
+            Console.WriteLine(result.Data.Reason);
+         }
+         catch (Exception)
+         {
 
-         //   throw;
-         //}
+            throw;
+         }
 
          //var resu = httpClientHuggy.Contacts.GetAsync(94331700).Result;
 
-         var model = new Huggy.Models.Contacts.ContactUpdate
-         {
-            Id = "93450618",
-            Name = "Eduardo Prietro",
-            Mobile = "5518996132391",
-            Phone = "5518996132391",
-            Email = "eduardo@s2viagens.com",
-            Obs = "Update now super..."
-         };
-         var resultC = httpClientHuggy.Contacts.PutAsync(model, 93450618).Result;
+         //var model = new Huggy.Models.Contacts.ContactUpdate
+         //{
+         //   Id = "93450618",
+         //   Name = "Eduardo Prietro",
+         //   Mobile = "5518996132391",
+         //   Phone = "5518996132391",
+         //   Email = "eduardo@s2viagens.com",
+         //   Obs = "Update now super..."
+         //};
+         //var resultC = httpClientHuggy.Contacts.PutAsync(model, 93450618).Result;
 
-         var result = httpClientHuggy.Contacts.GetListAsync().Result;
-         if (result.IsValid)
-         {
-            foreach (var r in result.Data)
-            {
-               Console.WriteLine("{0:00000000} - {1} {2} {3}", r.Id, r.Name, r.Phone, r.Mobile);
-            }
-         }
-         else
-         {
-            Console.WriteLine("Inválido");
-         }
+         //var result = httpClientHuggy.Contacts.GetListAsync().Result;
+         //if (result.IsValid)
+         //{
+         //   foreach (var r in result.Data)
+         //   {
+         //      Console.WriteLine("{0:00000000} - {1} {2} {3}", r.Id, r.Name, r.Phone, r.Mobile);
+         //   }
+         //}
+         //else
+         //{
+         //   Console.WriteLine("Inválido");
+         //}
 
       }
    }
